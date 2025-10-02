ab4ef826f4cc6e94edd9ab4fde5834c2cf10e1a4
yahianurhussien <yahianurhussien@gmail.com>
2025-10-02 14:47:31 +0300

    factorial using stream api

--- src/main/java/com/yahia/the_java_lab/core/Factorial.java
+++ src/main/java/com/yahia/the_java_lab/core/Factorial.java
@@ -1,5 +1,8 @@
 package com.yahia.the_java_lab.core;
 
+import java.math.BigInteger;
+import java.util.stream.IntStream;
+
 public class Factorial {
 
     // traditional way
@@ -16,6 +19,38 @@ public class Factorial {
         if (num == 1) return 1;
 
         return num * factorial(num - 1);
+
+    }
+
+    // using stream api
+    public static int steamFactorial(int num){
+        if(num < 0) throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+
+        return IntStream.rangeClosed(1,num)
+                .reduce( 1,(a,b) -> a*b ); // starting from 1 it multiplies a anb b
+
+    }
+
+    //factorial grows really fast for num > 20 it won't even fit in a long
+    // use BigInteger
+    public static BigInteger steamFactorialBigInteger(int num){
+        if(num < 0) throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+
+        return IntStream.rangeClosed(1,num)
+                .mapToObj(BigInteger::valueOf)
+                .reduce( BigInteger.ONE,(BigInteger::multiply)); // starting from 1 it multiplies a anb b
+
+    }
+
+    // we can use parallel stream for fast factorial calculation
+    public static BigInteger parallelStreamFactorial(int num){
+        if(num < 0) throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+
+        return IntStream.rangeClosed(1,num)
+                .parallel()//enable parallelism
+                .mapToObj(BigInteger::valueOf)
+                .reduce( BigInteger.ONE,(BigInteger::multiply)); // starting from 1 it multiplies a anb b
+
     }
 
 
@@ -23,5 +58,9 @@ public class Factorial {
 
         System.out.println(factorial(5));
         System.out.println(recursiveFactorial(5));
+        System.out.println(steamFactorial(5));
+        System.out.println(steamFactorialBigInteger(25));
+        System.out.println(parallelStreamFactorial(40));
+
     }
 }
