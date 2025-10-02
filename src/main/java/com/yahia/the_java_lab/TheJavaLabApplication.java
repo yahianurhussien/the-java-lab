53f3a08217c4a68c681c92e2ad58fa1974e84545
yahianurhussien <yahianurhussien@gmail.com>
2025-10-02 14:14:02 +0300

    initial commit

---
+++ src/main/java/com/yahia/the_java_lab/TheJavaLabApplication.java
@@ -0,0 +1,13 @@
+package com.yahia.the_java_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJavaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheJavaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yahia/the_java_lab/TheJavaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yahia.the_java_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
