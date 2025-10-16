ecc172c147e6c9dd00f3cd3942ee5f3d65b92eff
yahianurhussien <yahianurhussien@gmail.com>
2025-10-16 09:10:01 -0500

    palindrom check using chartAt

--- src/main/java/com/yahia/the_java_lab/core/Palindrome.java
+++ src/main/java/com/yahia/the_java_lab/core/Palindrome.java
@@ -10,9 +10,21 @@ public class Palindrome {
         return false;
     }
 
+
+    public static boolean isPalindromeLoop(String str){
+        for(int i = 0; i < Math.sqrt(str.length()); i++){
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         System.out.println(isPalindrome("Yahia"));
         System.out.println(isPalindrome("malalam"));
+
+        System.out.println(isPalindromeLoop("yahia"));
+        System.out.println(isPalindromeLoop("malalam"));
 
     }
 }
