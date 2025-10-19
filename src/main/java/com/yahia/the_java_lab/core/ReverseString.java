8c846f2d5d3de678a70893a552a99d4fc8a39d35
yahianurhussien <yahianurhussien@gmail.com>
2025-10-19 14:59:21 -0500

    reverse using for loop

--- src/main/java/com/yahia/the_java_lab/core/ReverseString.java
+++ src/main/java/com/yahia/the_java_lab/core/ReverseString.java
@@ -1,6 +1,7 @@
 package com.yahia.the_java_lab.core;
 
 import java.io.FilterOutputStream;
+import java.util.Arrays;
 
 public class ReverseString {
 
@@ -9,7 +10,21 @@ public class ReverseString {
 
     }
 
+    //using for loop
+    public static String reverseForLoop(String str){
+        char[] chars = str.toCharArray();
+
+        for( int i = 0; i < (chars.length)/2; i++){
+           char tmp = chars[i];
+           chars[i] = chars[chars.length - 1 - i];
+           chars[chars.length - 1 - i] = tmp;
+        }
+
+        return new String(chars);
+    }
+
     public static void main(String[] args) {
         System.out.println(reverseUsingStringBuilder("Yahia"));
+        System.out.println(reverseForLoop("Yahia"));
     }
 }
